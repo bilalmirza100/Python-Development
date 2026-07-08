21aa62b750d8652534dbe9a00a05cd976b41c9ff
bilalmirza100 <nawabmirza174@gmail.com>
2026-07-08 19:14:02 +0500

    Add files via upload

---
+++ t2.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = float(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+total_bill = bill + (tip / 100 * bill)
+print(f"Each person should pay: ${total_bill / people:.2f}")
